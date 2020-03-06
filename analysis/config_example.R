08ef0ea4e71c2fe8fcb49cd02eaaf65118159ddf
Marc Melliger <marc.melliger@iass-potsdam.de>
2020-03-06 12:06:20 +0100

    adding working version of plot app

---
+++ analysis/config_example.R
@@ -0,0 +1,4 @@
+
+# Set the file path to where your emlab result files are stored.
+# Use / throught and at the end
+emlab_results_directory <- "/Users/path/"
---
+++ analysis/scripts/init.R
@@ -0,0 +1,74 @@
+
+library(tidyverse)
+library(RColorBrewer)
+
+
+
+# Config ------------------------------------------------------------------
+
+config_file <- "config.R"
+if(!file.exists(config_file)){
+  stop("config.R not found. Please set up from example file")
+} else {
+  source(file = config_file)
+  
+}
+
+source(file = "scripts/util_functions.R")
+
+# Loading and preparing files ---------------------------------------------
+
+result_files <- get_result_files(emlab_results_directory)
+latest_files <- get_latest_result_files(result_files)
+  
+# TODO selection, maybe in Shiny app
+#id = "1583329851748"
+#scenario = "Scenario_NL"
+
+id <- latest_files %>% 
+  slice(1) %>% 
+  pull(id)
+
+scenario <- latest_files %>% 
+  slice(1) %>% 
+  pull(scenario)
+
+warning("Analysing latest result files")
+prefix = paste(id, scenario, sep = "-") 
+
+
+# Get raw results ---------------------------------------------------------
+
+raw_main_results <- read_delim(
+  file = get_filepath("main.csv"),
+  delim = ";",
+  col_types = cols(.default = "n"),
+  locale = locale(decimal_mark = ".", grouping_mark =  "'")) %>% 
+  arrange(iteration, tick)
+
+## TODO add other result files here
+
+
+
+
+# Common variables --------------------------------------------------------
+
+meta_cols <- c("iteration", "tick")
+iteration_min <- min(raw_main_results$iteration)
+iteration_max <- max(raw_main_results$iteration)
+
+
+# Cleanup -----------------------------------------------------------------
+
+rm(
+  emlab_results_directory,
+  config_file,
+  get_filepath,
+  latest_files,
+  result_files,
+  get_latest_result_files,
+  get_result_files
+)
+
+
+
